730447704c36df5d4283b1461b3ade79dfc6bb6d
Tanujnarnam <67031523+Tanujnarnam@users.noreply.github.com>
2024-02-02 12:14:54 -0600

    Finished Sign Up Page

--- front_end/liftlog/src/App.js
+++ front_end/liftlog/src/App.js
@@ -4,11 +4,13 @@ import Layout from './components/Layout'
 import Login from './components/Login';
 import Home from './components/Home';
 import Track from './components/Track';
+import Signup from './components/Signup';
 
 function App() {
   return (
     <Routes>
       <Route index path="/" element={<Login/>}/>
+      <Route path="/signup" element={<Signup/>}/>
       <Route path="/" element={<Layout />}>
         <Route path="home" element={<Home/>}/>
         <Route path="track" element={<Track/>}/>
--- front_end/liftlog/src/components/Signup/index.js
+++ front_end/liftlog/src/components/Signup/index.js
@@ -1,25 +1,23 @@
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faDumbbell } from "@fortawesome/free-solid-svg-icons";
-import './login.css'
+import './signup.css'
 
-const Login = () => {
+const Signup = () => {
   return(
     <>
     <form>
     <h1 className="logo">Liftlog <FontAwesomeIcon icon={faDumbbell} /></h1>
     <input type="text" autocomplete="off" name="text" className="input" placeholder="Username" required/>
     <input type="password" placeholder="Password" className="input" required/>
-    <Link to="/home" className="animated-button">
-      <span>Submit</span>
+    <input type="password" placeholder="Confirm Password" className="input" required/>
+    <button class="animated-button">
+      <span>Sign Up</span>
       <span></span>
-    </Link>
-    <Link to="signup" className="create-account">
-      <span>Create Account</span>
-    </Link>
+    </button>
     </form>
     </>
   )
 };
 
-export default Login;
+export default Signup;
