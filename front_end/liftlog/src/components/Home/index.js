3813a51e691b6cbfe48c837181cbfca4bd117a68
Tanujnarnam <67031523+Tanujnarnam@users.noreply.github.com>
2024-02-02 13:35:54 -0600

    Home Page Designed

--- front_end/liftlog/src/components/Home/index.js
+++ front_end/liftlog/src/components/Home/index.js
@@ -1,8 +1,20 @@
 import './home.css'
+import {Link} from 'react-router-dom'
 
 const Home = () => {
   return(
-    <h1>Hello</h1>
+    <div className="Home">
+      <h1>Welcome User!</h1>
+      <h2>Maximize your gains and track 
+        your progress like never before with our cutting-edge gym rep tracker.
+        Whether you're a seasoned athlete or just starting your fitness journey,
+        Liftlog is your ultimate companion for achieving your fitness 
+        goals.</h2>
+      <Link to="/track" className='track-link'>
+        <span>Get Started</span>
+        <span></span>
+      </Link>
+    </div>
   )
 };
 
