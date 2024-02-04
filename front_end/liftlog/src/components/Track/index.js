9fcf9593f9ab9a4b64569766fb02404b2b1ccfbf
Tanujnarnam <67031523+Tanujnarnam@users.noreply.github.com>
2024-02-03 19:00:25 -0600

    Track Page Design Done

--- front_end/liftlog/src/components/Track/index.js
+++ front_end/liftlog/src/components/Track/index.js
@@ -1,7 +1,33 @@
+import { Link } from 'react-router-dom';
+import './track.css'
 
 const Track = () => {
   return(
-    <h1>Hello</h1>
+    <>
+    <div className='Track'>
+      <div className='track-div'>
+        <label>Exercise</label>
+        <select name="selectedFruit">
+          <option value="ibp">Inclined Bench Press</option>
+          <option value="pdf">Peck Deck Flies</option>
+          <option value="lr">Lateral Raises</option>
+          <option value="rdf">Rear Delt Flies</option>
+        </select>
+      </div>
+      <div className='track-div'>
+        <label>Weight(lbs)</label>
+        <input className="track-input"></input>
+      </div>
+      <div className='track-div'>
+        <label>Reps</label>
+        <input className="track-input"></input>
+      </div>
+    </div>
+    <Link to="/home" className="animated-button2">
+      <span>Track</span>
+      <span></span>
+    </Link>
+    </>
   )
 }
 
