eeefa0d9f2bd5d12c0341b67c038d97506b0ce26
Tanujnarnam <67031523+Tanujnarnam@users.noreply.github.com>
2024-02-06 14:54:09 -0600

    Backend Routes for Tracking and Viewing Data Done

--- back_end/routes/dashboard.js
+++ back_end/routes/dashboard.js
@@ -15,4 +15,29 @@ router.get("/", authorization, async(req,res) => {
   }
 })
 
+router.post("/track", authorization, async (req, res) => {
+  try{
+    const {exercise, weight, reps} = req.body;
+    const new_date = Date(Date.now()).toString();
+
+    const response = await pool.query("INSERT INTO data (exercise, weight, reps, date, user_id) VALUES ($1,$2,$3,$4,$5)", [exercise, weight, reps, new_date.slice(4, 15), req.id]);
+
+    res.status(200).send("Entry Logged");
+  }catch(err){
+    console.log(err.message);
+    res.status(500).send("Server Error");
+  }
+})
+
+router.get("/view", authorization, async (req, res) => {
+  try{
+    const response = await pool.query("SELECT * FROM data WHERE user_id = $1 ORDER BY date DESC",[req.id]);
+  
+    res.send(response.rows);
+  }catch(err){
+    console.log(err.message);
+    res.status(500).send("Server Error");
+  }
+})
+
 export default router;
