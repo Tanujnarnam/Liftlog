ebf717025b38f9a36e29777d347cd920933dc757
Tanujnarnam <67031523+Tanujnarnam@users.noreply.github.com>
2024-02-07 20:34:23 -0600

    Setting up Environment Variables

--- back_end/database.js
+++ back_end/database.js
@@ -3,10 +3,10 @@ import pg from 'pg'
 const Pool = pg.Pool
 
 const pool = new Pool({
-  user: "mmjbngaz",
-  password: "beI58qWzTb4rB64_RXeYU3pEPAFHv0Jr",
-  host:"kashin.db.elephantsql.com",
-  database:"mmjbngaz"
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  host: process.env.HOST,
+  database: process.env.DATABASE
 })
 
 export default pool;
